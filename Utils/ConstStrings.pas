c663fbd648dab1faf54872ae6fb36b08bba6c08c
ZRazor <exp.razor@gmail.com>
2013-03-09 01:17:52 +1100

    0.9.3 fix

--- Utils/ConstStrings.pas
+++ Utils/ConstStrings.pas
@@ -24,7 +24,7 @@ const
   SYS_SETTINGS_FOLDER = 'settings\';
   SYS_KEEP2ME = 'Keep2Me';
   SYS_USERAGENT = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)';
-  SYS_UPDATE_CHECK_PAGE = 'http://keep2.me/program/lastversion2.php?clientversion=' + SYS_KEEP_VERSION;
+  SYS_UPDATE_CHECK_PAGE = 'http://keep2.me/program/lastversion.php?clientversion=' + SYS_KEEP_VERSION;
   SYS_UPDATE_TOKEN = 'K2M_VERSION';
   SYS_UPDATER_EXE_NAME = 'updater.exe';
 
